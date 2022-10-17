347f032d53a3b324ad4b68d16d29d0a2f18709f7
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-17 14:28:26 -0700

    Add multiplication functionality to scripts.js
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,16 @@ function subtract(number1, number2) {
   return number1 - number2;
 }
 
+// Multiplication
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
 
 // User Interface logic
 const number1 = parseInt(prompt("Enter a number"));
 const number2 = parseInt(prompt("Enter another number"));
 
+window.alert(multiply(number1,number2))
 window.alert(subtract(number1,number2))
 window.alert(add(number1,number2))
