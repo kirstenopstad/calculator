12ff8cb1c0de20bb071efe1fd269be7330860540
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-17 14:30:59 -0700

    Add division functionality to scripts.js
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,17 @@ function multiply(number1, number2) {
   return number1 * number2;
 }
 
+//Division
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 
 // User Interface logic
 const number1 = parseInt(prompt("Enter a number"));
 const number2 = parseInt(prompt("Enter another number"));
 
+window.alert(divide(number1, number2))
 window.alert(multiply(number1,number2))
 window.alert(subtract(number1,number2))
 window.alert(add(number1,number2))
