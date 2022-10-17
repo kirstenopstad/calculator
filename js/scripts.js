fceb1bf05f204d9e6b33586606efb14756c41523
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-17 14:24:06 -0700

    Add subtraction functionality to scripts.js
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 // Business logic
+
+// Addition
 function add(number1, number2) {
   return number1 + number2;
 }
+
+// Subtraction
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
 
 // User Interface logic
 const number1 = parseInt(prompt("Enter a number"));
 const number2 = parseInt(prompt("Enter another number"));
 
+window.alert(subtract(number1,number2))
 window.alert(add(number1,number2))
